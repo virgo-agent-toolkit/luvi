37605bbd5a1e574ed97b67c3a142d8bbfff30061
Geoff Bourne <geoff.bourne@rackspace.com>
2019-11-14 09:53:35 -0600

    Try APPEND LIB_LIST for WIN32 extra libs

--- make.bat
+++ make.bat
@@ -45,7 +45,7 @@ GOTO :end
 
 :build
 IF NOT EXIST build CALL Make.bat regular
-cmake --build build --config Release
+cmake --build build --config Release -- /maxcpucount
 COPY build\Release\luvi.exe .
 GOTO :end
 
